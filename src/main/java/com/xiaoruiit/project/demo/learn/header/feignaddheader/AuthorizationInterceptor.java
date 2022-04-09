f33f3f333a45d1ae13e51072f3eab11ffb5b7b91
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2022-04-09 18:50:53 +0800

    feat: feign添加请求头

---
+++ src/main/java/com/xiaoruiit/project/demo/learn/header/feignaddheader/AuthorizationInterceptor.java
@@ -0,0 +1,40 @@
+package com.xiaoruiit.project.demo.learn.header.feignaddheader;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author hxr
+ * @since 2022/4/9
+ */
+@Slf4j
+@Component
+public class AuthorizationInterceptor extends HandlerInterceptorAdapter {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        /**
+         * 每次请求解析token放入对象，并存到当前线程的变量ThreadLocal
+         */
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserCode("U123");
+        userInfo.setUserName("admin");
+        UserInfo.set(userInfo);
+
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        /**
+         * 请求结束清除线程变量
+         */
+        UserInfo.clean();
+    }
+}
