38d22088d795ec09ad4817c55dfb06471d90abdf
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2022-04-19 15:47:06 +0800

    feat: 优化调用者

--- src/main/java/com/xiaoruiit/project/demo/learn/header/callerinfo/CallerInfoInterceptor.java
+++ src/main/java/com/xiaoruiit/project/demo/learn/header/callerinfo/CallerInfoInterceptor.java
@@ -27,6 +27,7 @@ public class CallerInfoInterceptor extends HandlerInterceptorAdapter {
 
     /**
      * 请求被处理前，拦截器拦截，提取调用者信息，放入local
+     *
      * @param request
      * @param response
      * @param handler
@@ -35,18 +36,19 @@ public class CallerInfoInterceptor extends HandlerInterceptorAdapter {
      */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String userName = null;
+        String userCode = null;
         try {
-            if ("dev".equals(env) && request.getHeader("userCode") == null){
-                CallerInfo callerInfo = new CallerInfo();
-                callerInfo.setUserCode("U123");
-                callerInfo.setUserName("hxr");
-                CallerInfo.setCallerInfo(callerInfo);
+            if ("dev".equals(env) && request.getHeader("userCode") == null) {
+                userName = "U123";
+                userCode = "hxr";
 
                 return true;
+            } else if (request.getHeader("userCode") != null) {
+                userName = URLDecoder.decode(request.getHeader("userName"), StandardCharsets.UTF_8.toString());// URLDecoder解决乱码
+                userCode = request.getHeader("userCode");
+                log.warn("调用者请求参数：{}",request);
             }
-
-            String userName = URLDecoder.decode(request.getHeader("userName"), StandardCharsets.UTF_8.toString());// URLDecoder解决乱码
-            String userCode = request.getHeader("userCode");
 
             CallerInfo callerInfo = new CallerInfo();
             callerInfo.setUserCode(userCode);
@@ -56,14 +58,15 @@ public class CallerInfoInterceptor extends HandlerInterceptorAdapter {
             log.debug(CallerInfo.getCallerInfo().toString());
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
-        } catch (NullPointerException e){
-            log.error("获取上游调用者失败，{}",e);
+        } catch (NullPointerException e) {
+            log.error("获取上游调用者失败，{}", e);
         }
         return true;
     }
 
     /**
      * 调用结束后清除线程中调用者信息，防止在线程池中出问题
+     *
      * @param request
      * @param response
      * @param handler
