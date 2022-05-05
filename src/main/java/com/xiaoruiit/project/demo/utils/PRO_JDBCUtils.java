5de53f84029604d3f0b9b4ce1cdc8d5cdc3326e6
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2022-05-05 11:51:30 +0800

    feat: jdbc工具类

---
+++ src/main/java/com/xiaoruiit/project/demo/utils/PRO_JDBCUtils.java
@@ -0,0 +1,35 @@
+package com.xiaoruiit.project.demo.utils;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+
+/**
+ * JdbcTemplate工具类
+ *
+ * @author hxr
+ */
+
+@Slf4j
+public class PRO_JDBCUtils {
+    //  type: com.alibaba.druid.pool.DruidDataSource
+    //    driverClassName: com.mysql.cj.jdbc.Driver
+    static String url = "jdbc:mysql://127.0.0.1?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true";
+    static String username = "root";
+    static String password = "123456";
+
+    public static JdbcTemplate getJdbcTemplate(){
+        // Spring 内置连接池
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        // 需要 jdbc 四个参数
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        return jdbcTemplate;
+    }
+
+}
