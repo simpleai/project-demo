c8e60ecc722218497d5842bfc98195ffbcee1af5
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2022-07-08 16:20:35 +0800

    fix: @Value 和 数据库配置 导致无法启动

--- src/main/java/com/xiaoruiit/project/demo/DemoApplication.java
+++ src/main/java/com/xiaoruiit/project/demo/DemoApplication.java
@@ -3,14 +3,22 @@ package com.xiaoruiit.project.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.util.Arrays;
 
 /**
  * @author hanxiaorui
  * @date 2021/12/22
  */
+@EnableFeignClients
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class DemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        // @Value 读取配置
+        args = Arrays.copyOf(args, args.length + 1);
+        args[args.length - 1] = "--spring.cloud.bootstrap.enabled=true";
+        SpringApplication.run(DemoApplication.class,args);
+
     }
 }
