a4ef7c3cac8f2c7d68254579c199ac8a4d86fec8
hanxiaorui <hanxiaorui@1623668083884741.onaliyun.com>
2022-12-28 12:01:29 +0800

    feat: 未验证url工具类

---
+++ src/main/java/com/xiaoruiit/project/demo/utils/RequestUtil.java
@@ -0,0 +1,99 @@
+package com.xiaoruiit.project.demo.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * 提供全局的request 对象
+ * todo 验证
+ */
+@Slf4j
+public class RequestUtil {
+
+    private RequestUtil() {
+    }
+
+    /**
+     * 得到当前的请求对象
+     *
+     * @return
+     */
+    public static HttpServletRequest getHttpServletRequest() {
+        return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+    }
+
+    /**
+     * 得到当前请求的ip
+     *
+     * @return
+     */
+    public static String getRequestIpAddress() {
+        HttpServletRequest httpServletRequest = getHttpServletRequest();
+        if (httpServletRequest != null) {
+            return getIpAddr(httpServletRequest);
+        }
+        return "";
+    }
+
+
+    /**
+     * 得到ip地址
+     *
+     * @param request
+     * @return
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ipAddress;
+        try {
+            ipAddress = request.getHeader("x-forwarded-for");
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getRemoteAddr();
+                if (ipAddress.equals("127.0.0.1")) {
+                    // 根据网卡取本机配置的IP
+                    InetAddress inet = null;
+                    try {
+                        inet = InetAddress.getLocalHost();
+                    } catch (UnknownHostException e) {
+                        log.error(e.getMessage(), e);
+                    }
+                    ipAddress = inet.getHostAddress();
+                }
+            }
+            // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+            if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
+                // = 15
+                if (ipAddress.indexOf(",") > 0) {
+                    ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+                }
+            }
+        } catch (Exception e) {
+            ipAddress = "";
+        }
+        return ipAddress;
+    }
+
+    /**
+     * 得到当前请求的URL
+     *
+     * @return
+     */
+    public static String getRequestUrl() {
+        HttpServletRequest httpServletRequest = getHttpServletRequest();
+        if (httpServletRequest != null) {
+            return httpServletRequest.getRequestURI();
+        }
+        return "";
+    }
+
+}
